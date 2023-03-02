faca1e4917ad9c8106a9122d642c9bd2490dc9c3
QuynhAn <124511413+thegoofybaddie@users.noreply.github.com>
2023-03-02 21:57:33 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
+a = st.number_input('Tham số a')
+b = st.number_input('Tham số b')
+if st.button('Giải'):
+  st.success(f'Phương trình có {a} nghiệm là {b}')
