b7e30f5c33237f1efd2638bd09c924e338198c3d
QuynhAn <124511413+thegoofybaddie@users.noreply.github.com>
2023-03-02 22:04:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 if st.button('Giải'):
-  st.success(f'Phương trình có {a} nghiệm là {b}')
+  st.success(f'Phương trình có 1 nghiệm là -2.0')
