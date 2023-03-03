385c23413baf6c45317e6c52280602a49fd86d65
QuynhAn <124511413+thegoofybaddie@users.noreply.github.com>
2023-03-03 21:42:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 if st.button('Giải'):
-  st.success('Phương trình có 1 nghiệm là -2.0')
+  result = -b/a
+  st.success(f'Phương trình có 1 nghiệm là {result}' )
