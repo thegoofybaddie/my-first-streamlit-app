ef5e96d0f69031bb86a6987fcded11aa5e9d5351
QuynhAn <124511413+thegoofybaddie@users.noreply.github.com>
2023-03-16 23:38:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ import sklearn
 import plotly
 
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
-a = st.number_input('Tham số a')
-b = st.number_input('Tham số b')
-if st.button('Giải'):
-  if a == 0 and b == 0:
-    st.success('Phương trình có vô số nghiệm')
-  elif a == 0 and b!= 0:
-    st.success('Phương trình vô nghiệm')
-  else:
-    result = -b/a
-    st.success(f'Phương trình có 1 nghiệm là {result}' )
+a = st.number_input('Input Temperature')
+if st.button('Predict'):
+  if :
+    st.success('')
+  
